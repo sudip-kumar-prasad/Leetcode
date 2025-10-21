54bdafa69a323eae794ca92b7a22c46faf57a555
Sudip Kumar Prasad <sudipkumarprasad2005@gmail.com>
2025-10-21 09:21:52 +0530

    Time: 994 ms (28.57%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select 
+    d.name as Department,
+    e.name as Employee,
+    e.salary as Salary
+from Employee e
+join Department d
+on e.departmentId = d.id
+where 
+    e.salary = (
+        SELECT MAX(salary)
+        FROM Employee
+        WHERE departmentId = e.departmentId
+    );;
+
+
