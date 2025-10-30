a1a5a7ff36a3915c7f1c4a9ed2a40608d7ac2ec1
Sudip Kumar Prasad <sudipkumarprasad2005@gmail.com>
2025-10-30 18:47:17 +0530

    Time: 4438 ms (5.08%), Space: 211.5 MB (5.08%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        n=len(prices)
+        ans = 0
+        memo = {}
+        def helper(idx,buy,t_count):
+            if idx == n or t_count == 2:
+                return 0
+            if (idx,buy,t_count) in memo:
+                return memo[(idx,buy,t_count)]
+            if buy:
+                take = -prices[idx] + helper(idx+1,not buy,t_count)
+                not_take = helper(idx+1, buy,t_count)
+                ans = max(take,not_take)
+            else:
+                take = prices[idx] + helper(idx+1,not buy,t_count+1)
+                not_take = helper(idx+1,buy,t_count)
+                ans = max(take,not_take)
+            memo[(idx,buy,t_count)] = ans
+            return ans
+        return helper(0,True,0)
