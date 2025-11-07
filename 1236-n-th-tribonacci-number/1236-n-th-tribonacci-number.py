e758c224633cb54ed4cfcc877344ed46a1ee1707
Sudip Kumar Prasad <sudipkumarprasad2005@gmail.com>
2025-11-07 23:10:30 +0530

    Time: 0 ms (100%), Space: 12.5 MB (53.05%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def tribonacci(self, n):
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+
+        a, b, c = 0, 1, 1
+        for i in range(3, n + 1):
+            a, b, c = b, c, a + b + c
+
+        return c
