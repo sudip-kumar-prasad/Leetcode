4b234e5a0e53c5cc581947781bc3dc6d62cf966a
Sudip Kumar Prasad <sudipkumarprasad2005@gmail.com>
2026-04-08 13:02:40 +0530

    Time: 49 ms (25.14%), Space: 52.6 MB (94.7%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,18 @@
+var expect = function(val) {
+    return {
+        toBe: function(other) {
+            if (val === other) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(other) {
+            if (val !== other) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+};
