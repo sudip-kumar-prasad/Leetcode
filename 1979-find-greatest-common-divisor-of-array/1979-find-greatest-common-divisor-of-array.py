d41c14becf6db88b4c44dc2ead872ac1bbe7bb71
Sudip Kumar Prasad <sudipkumarprasad2005@gmail.com>
2026-04-12 00:11:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (38.91%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
@@ -3,31 +3,34 @@ class Solution(object):
     def findGCD(self, nums):
         l = min(nums)
         h = max(nums)
-        x = []
-        y = []
-        for i in range(1,l+1):
-            if l%i == 0:
-                x.append(i)
-        for i in range(1,h+1):
-            if h%i == 0:
-                y.append(i)
+        # x = []
+        # y = []
+        # for i in range(1,l+1):
+        #     if l%i == 0:
+        #         x.append(i)
+        # for i in range(1,h+1):
+        #     if h%i == 0:
+        #         y.append(i)
 
-        l1 = len(x)
-        l2 = len(y)
+        # l1 = len(x)
+        # l2 = len(y)
 
-        res = float('-inf')
-        if l1<l2:
-            for i in x:
-                if i in y:
-                    if i>res:
-                        res = i
-        else:
-            for i in y:
-                if i in x:
-                    if i>res:
-                        res = i
+        # res = float('-inf')
+        # if l1<l2:
+        #     for i in x:
+        #         if i in y:
+        #             if i>res:
+        #                 res = i
+        # else:
+        #     for i in y:
+        #         if i in x:
+        #             if i>res:
+        #                 res = i
 
-        return res
+        # return res
+        while h!=0:
+            l,h=h,(l%h)
+        return l
 
 
 
